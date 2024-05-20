4435819cda3f1cb1458f9f73faba4a5746170c68
syedzawadarefin <zawadarefin0@gmail.com>
2024-05-20 20:54:48 +1000

    added cart page

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-  }
-
-
-  document.getElementById("formcontents").addEventListener("submit", enterForm);
+}
 
 function reservesuccess() {
     alert("Successfully booked a seat.")
 }
 
-
-
 function cartsuccess() {
     alert("Added to cart")
-
 }
 
-
-function addtocart() {
-    document.getElementsByClassName("cart-button-text").innerHTML = "Added to cart"
-
-}
-
-const popup = document.querySelector(".full-screen")
-
-function togglePopup() {
-    popup.classList.toggle('hidden')
+function accountsuccess() {
+    alert("Successfully logged in!")
 }
