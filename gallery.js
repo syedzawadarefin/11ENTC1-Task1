f8dfd533c6ef11f0ae3b129d9de40b0d7c0b9d1e
syedzawadarefin <zawadarefin0@gmail.com>
2024-05-20 17:13:29 +1000

    made very disgusting gallery page with enlarging functionality on click

---
+++ gallery.js
@@ -0,0 +1,30 @@
+// absolutely beautiful i know
+const popup1 = document.querySelector('.one')
+
+function togglePopup1() {
+    popup1.classList.toggle('hidden')
+}
+
+const popup2 = document.querySelector('.two')
+
+function togglePopup2() {
+    popup2.classList.toggle('hidden')
+}
+
+const popup3 = document.querySelector('.three')
+
+function togglePopup3() {
+    popup3.classList.toggle('hidden')
+}
+
+const popup4 = document.querySelector('.four')
+
+function togglePopup4() {
+    popup4.classList.toggle('hidden')
+}
+
+const popup5 = document.querySelector('.five')
+
+function togglePopup5() {
+    popup5.classList.toggle('hidden')
+}
--- script.js
+++ script.js
@@ -2,28 +2,28 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }
 
-function search() {
-    var input, filter, li, ul, a, i;
-    input = document.getElementById("query");
-    filter = input.value.toUpperCase();
-    console.log(filter)
-    li = document.getElementById("menu-item");
-    console.log(li)
-    ul = document.getElementsByClassName("item-text");
-    console.log(ul)
 
-    console.log(li.length)
-    for (i = 0; i < li.length; i++) {
-        console.log("yes!")
-        a = li[i].getElementsByTagName("span")[0];
-        if (a.children[0].children[0].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none"
-        }
-    }
+  document.getElementById("formcontents").addEventListener("submit", enterForm);
+
+function reservesuccess() {
+    alert("Successfully booked a seat.")
 }
+
+
+
+function cartsuccess() {
+    alert("Added to cart")
+
+}
+
 
 function addtocart() {
     document.getElementsByClassName("cart-button-text").innerHTML = "Added to cart"
+
+}
+
+const popup = document.querySelector(".full-screen")
+
+function togglePopup() {
+    popup.classList.toggle('hidden')
 }
